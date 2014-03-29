9e0e2c4fdd55c54b0fbe8ec57b0e3ed50fe3440e
Alig96 <aliaslam_10@hotmail.co.uk>
2014-03-29 23:03:28 +0000

    Update config.lua

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -7,7 +7,7 @@ if SERVER then
 	
 	//Downloads
 	resource.AddWorkshop("182104437")
-	resource.AddWorkshop("132835998")
+	--resource.AddWorkshop("132835998")
 
 	resource.AddFile( "models/toybox.mdl" )
 	resource.AddFile( "materials/models/toybox/zombie_treasure_box_c.vmt" )
@@ -20,22 +20,30 @@ if SERVER then
 	
 	
 	//MAIN CONFIG
+	
+	//The first wave of zombies
 	bnpvbWJpZXM.Config.BaseDifficultySpawnRateCurve = 5
+	//Difficulty of the curve
 	bnpvbWJpZXM.Config.DifficultySpawnRateCurve = 1.01
-
+	//Base health at level 1
 	bnpvbWJpZXM.Config.BaseDifficultyHealthCurve = 75
+	//Difficulty of the curve
 	bnpvbWJpZXM.Config.DifficultyHealthCurve = 0.6
-
+	
+	//Max amount of zombies at the same time
 	bnpvbWJpZXM.Config.MaxZombiesSim = 100
 	
+	//Self Explanitory
 	bnpvbWJpZXM.Config.BaseStartingPoints = 500
 	bnpvbWJpZXM.Config.BaseStartingWeapon = "weapon_sim_colt1911"
 	bnpvbWJpZXM.Config.BaseStartingAmmoAmount = 120
 	
 	//Change name variables
+	//Setting this to true allows for the gamemode change the name to have a tag in front while its going on
 	bnpvbWJpZXM.Config.AllowServerName = true
 	bnpvbWJpZXM.Config.ServerName =	GetHostName()
 	bnpvbWJpZXM.Config.ServerNameProg = "[In Progress] "
+	//Similar to Allow Server name, but it also locks the server.
 	bnpvbWJpZXM.Config.AllowServerPasswordLocking = false
 	
 end
