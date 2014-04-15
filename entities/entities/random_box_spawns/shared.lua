2f8664b1f0ba182ee93b6c890f12239026039134
Madkiller Max <madkillermax@live.com>
2014-04-15 08:05:39 -0600

    No physics needed!
    Doesn't even need to be solid!

--- entities/entities/random_box_spawns/shared.lua
+++ entities/entities/random_box_spawns/shared.lua
@@ -10,17 +10,11 @@ ENT.Instructions	= ""
 
 
 function ENT:Initialize()
-	 
-		self:SetModel( "models/toybox.mdl" )
-		self:PhysicsInit( SOLID_VPHYSICS )
-		self:SetColor( 255, 255, 255 )
-		self:SetMoveType( MOVETYPE_NONE )
-		self:SetSolid( SOLID_VPHYSICS )
-		self:DrawShadow( false )
-		local phys = self:GetPhysicsObject()
-		if (phys:IsValid()) then
-			phys:Wake()
-		end
+	self:SetModel( "models/toybox.mdl" )
+	self:SetColor( 255, 255, 255 )
+	self:SetMoveType( MOVETYPE_NONE )
+	self:SetSolid( SOLID_NONE )
+	self:DrawShadow( false )
 end
 
 if CLIENT then
