84aa768ab596f87859ea292bc56cc16a560dac31
Madkiller Max <madkillermax@live.com>
2014-07-20 19:04:41 -0600

    nil function

--- gamemode/powerups/sv_powerups.lua
+++ gamemode/powerups/sv_powerups.lua
@@ -32,6 +32,9 @@ function nz.PowerUps.Set(id, bool, time, name, material)
 	data.time = time or data.time or -1
 	data.name = name or data.name or "*UNKNOWN*"
 	data.material = material or data.material or ""
+	if (CLIENT) then
+		data.func = nil
+	end
 	nz.PowerUps.buffer[id] = data
 	net.Start("nz_PowerUps_Sync")
 		net.WriteString(id)
