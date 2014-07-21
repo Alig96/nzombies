84d5da552c9295eba5c8868f0649963e49f7921c
Madkiller Max <madkillermax@live.com>
2014-07-20 18:41:12 -0600

    Lets try this

--- gamemode/enemies/sv_enemies.lua
+++ gamemode/enemies/sv_enemies.lua
@@ -1,30 +1,30 @@
-function OnEnemyKilled( enemy, attacker )
-
+function OnEnemyKilled(enemy, attacker)
 	if attacker:IsPlayer() then
 		attacker:GivePoints(90)
+		attacker:AddFrags(1)
 	end
 	nz.Rounds.CurrentZombies = nz.Rounds.CurrentZombies - 1
 	nz.Rounds.ZombiesSpawned = nz.Rounds.ZombiesSpawned - 1
 
-	local function createPowerup(pos)
-		local ent1 = ents.Create("drop_powerups") 
-		local powerups = {}
-		for k,_ in pairs(nz.PowerUps.GetBufferAll()) do
-			table.insert(powerups, k)
+	local powerData = false
+	for id,data in pairs(nz.PowerUps.GetBufferAll()) do
+		if (math.Rand(0, 100) <= data.chance) then
+			powerData = data
+			break
 		end
-		local rand = table.Random(powerups) or "dp"
-		ent1.Buff = rand
-		ent1:SetModel( nz.PowerUps.GetBuffer(rand).model )
+	end
+	if (powerData) then
+		local pos = enemy:GetPos()+Vector(0,0,50)
+		local ent1 = ents.Create("drop_powerups")
+		ent1.Buff = powerData.id
 		pos.z = pos.z - ent1:OBBMaxs().z
-		ent1:SetPos( pos )
+		ent1:SetModel(powerData.model)
+		ent1:SetPos(pos)
 		ent1:Spawn()
 	end
-	if math.random(1,25) == 1 then createPowerup(enemy:GetPos()+Vector(0,0,50)) end
-
 end
 
-
-function OnEnemyHurt( enemy, attacker, hitgroup )
+function OnEnemyHurt(enemy, attacker)
 	if attacker:IsPlayer() then
 		attacker:GivePoints(10)
 	end
