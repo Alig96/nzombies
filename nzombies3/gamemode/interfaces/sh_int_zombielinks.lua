d29a9a1d3c5bd5c18ec3a5a8c9bb5ff68fe810ef
Ken Koch <kkoch986@gmail.com>
2015-09-10 21:42:54 -0400

    add max link to say zzombies cant spawn at this location once a certain link has passed.

--- nzombies3/gamemode/interfaces/sh_int_zombielinks.lua
+++ nzombies3/gamemode/interfaces/sh_int_zombielinks.lua
@@ -3,9 +3,13 @@
 if SERVER then
 	function nz.Interfaces.Functions.ZombLinkHandler( ply, data )
 		if ply:IsSuperAdmin() then
+			PrintTable(data)
 			data.ent.link = data.link
+			data.ent.max_link = data.max_link
+			data.ent.maxlink = data.max_link
 			//For the link displayer
 			data.ent:SetLink(data.link)
+			data.ent:SetMaxLink(data.max_link)
 		end
 	end
 end
@@ -17,10 +21,12 @@ if CLIENT then
 		local valz = {}
 		valz["Row1"] = 0
 		valz["Row2"] = 1
+		valz["Row3"] = -1
 		//Check if the ent has flags already
 		if data.link != nil then
 			valz["Row1"] = 1
 			valz["Row2"] = data.link
+			valz["Row3"] = data.max_link
 			name = "Modifying Zombie Link"
 		end
 
@@ -47,6 +53,11 @@ if CLIENT then
 		Row2:SetValue( valz["Row2"] )
 		Row2.DataChanged = function( _, val ) valz["Row2"] = val end
 
+		local Row3 = DProperties:CreateRow( "Zombie Spawn", "Max Flag" )
+		Row3:Setup( "Integer" )
+		Row3:SetValue( valz["Row3"] )
+		Row3.DataChanged = function( _, val ) valz["Row3"] = val end
+
 		local DermaButton = vgui.Create( "DButton" )
 		DermaButton:SetParent( DermaPanel )
 		DermaButton:SetText( "Submit" )
@@ -60,6 +71,13 @@ if CLIENT then
 				str=valz["Row2"]
 			end
 			data.link = str
+
+			if valz["Row1"] == 0 then
+				str=nil
+			else
+				str=valz["Row3"]
+			end
+			data.max_link = str
 			nz.Interfaces.Functions.SendRequests( "ZombLink", data )
 
 			DermaPanel:Close()
