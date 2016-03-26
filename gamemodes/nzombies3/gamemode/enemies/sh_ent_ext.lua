a51e90c555fd40916268fa651e95e3851e15b02a
Lolleko <larry.junglas@hotmail.de>
2016-03-26 17:51:38 +0100

    Fixed constant values

--- gamemodes/nzombies3/gamemode/enemies/sh_ent_ext.lua
+++ gamemodes/nzombies3/gamemode/enemies/sh_ent_ext.lua
@@ -1,8 +1,11 @@
 TARGET_PRIORITY_NONE = 0
 TARGET_PRIORITY_PLAYER = 1
 TARGET_PRIORITY_SPECIAL = 2
-TARGET_PRIORITY_MAX = 3
---Someone could add a new priority level by doing TARGET_PRIORITY_CUSTOM = TARGET_PRIORITY_MAX + 1
+TARGET_PRIORITY_MAX = 2
+-- Someone could add a new priority level by doing this:
+-- TARGET_PRIORITY_CUSTOM = TARGET_PRIORITY_MAX + 1
+-- TARGET_PRIORITY_MAX = TARGET_PRIORITY_MAX + 1
+-- would be limited to 7 custom levels before overwritting TARGET_PRIORITY_ALWAYS, which shoiuld be enough.
 TARGET_PRIORITY_ALWAYS = 10 --make this entity a global target (not recommended)
 
 --WARNING HTIS IS ONLY PARTIALLY SHARED its not recommended to use it clientside.
