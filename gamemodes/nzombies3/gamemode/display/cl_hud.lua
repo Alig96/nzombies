a46327990f87f3c6e317529562651e78d9a0bfb4
Zet0rz <zet0rmail2@gmail.com>
2016-04-04 23:23:33 +0200

    Update cl_hud.lua

--- gamemodes/nzombies3/gamemode/display/cl_hud.lua
+++ gamemodes/nzombies3/gamemode/display/cl_hud.lua
@@ -39,7 +39,7 @@ local tbl = {Entity(3), Entity(1), Entity(3), Entity(4), Entity(5),}
 
 local function ScoreHud()
 	if GetConVar("cl_drawhud"):GetBool() then
-		if true then --Round:InProgress() then
+		if Round:InProgress() then
 
 			local scale = (ScrW() / 1920 + 1) / 2
 			local offset = 0
