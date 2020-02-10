57ce7335af069ca2eee30332230bfdca341c6fef
Logan <lobsterlogan43@yahoo.com>
2020-02-10 00:14:09 -0600

    Update 7 - Really Shaping Up

---
+++ lua/autorun/client/rp_nlhh_v2u_clienteffects.lua
@@ -0,0 +1,148 @@
+function draw.Circle( x, y, radius, seg )
+    local cir = {}
+
+    table.insert( cir, { x = x, y = y, u = 0.5, v = 0.5 } )
+    for i = 0, seg do
+        local a = math.rad( ( i / seg ) * -360 )
+        table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+    end
+
+    local a = math.rad( 0 ) -- This is needed for non absolute segment counts
+    table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+
+    surface.DrawPoly( cir )
+end
+
+net.Receive("RunFireOverlay", function()
+    Screen = vgui.Create("DFrame")
+    Screen:SetSize(ScrW(), ScrH())
+    Screen:SetPos(0, 0)
+    Screen:SetTitle("")
+    Screen:SetVisible(true)
+    Screen:SetDraggable(false)
+    Screen:ShowCloseButton(false)
+    Screen.Paint = function()
+    end
+
+    local overlay = surface.GetTextureID("effects/fire/napalm_aoe")
+    overlayPanel = vgui.Create("DPanel", Screen)
+    overlayPanel:SetSize(Screen:GetWide(), Screen:GetTall())
+    overlayPanel:SetPos(0, 0)
+    overlayPanel.Paint = function()
+        if overlay then
+            surface.SetDrawColor(255, 255, 255)
+            surface.SetTexture(overlay)
+            surface.DrawTexturedRect(0, 0, overlayPanel:GetWide(), overlayPanel:GetTall())
+        end
+    end
+end)
+
+net.Receive("RunTeleportOverlay", function()
+    Screen = vgui.Create("DFrame")
+    Screen:SetSize(ScrW(), ScrH())
+    Screen:SetPos(0, 0)
+    Screen:SetTitle("")
+    Screen:SetVisible(true)
+    Screen:SetDraggable(false)
+    Screen:ShowCloseButton(false)
+    Screen.Paint = function()
+    end
+
+    local overlay = surface.GetTextureID("effects/electricity/bolt_sizzle")
+    overlayPanel = vgui.Create("DPanel", Screen)
+    overlayPanel:SetSize(Screen:GetWide(), Screen:GetTall())
+    overlayPanel:SetPos(0, 0)
+    overlayPanel.Paint = function()
+        if overlay and overlayPanel and overlayPanel:IsValid() then
+            surface.SetDrawColor(255, 255, 255)
+            surface.SetTexture(overlay)
+            surface.DrawTexturedRect(0, 0, overlayPanel:GetWide(), overlayPanel:GetTall())
+        end
+    end
+
+    overlayPanel2 = vgui.Create("DPanel", Screen)
+    overlayPanel2:SetSize(Screen:GetWide(), Screen:GetTall())
+    overlayPanel2:SetPos(0, 0)
+    overlayPanel2.Paint = function()
+    end
+
+    LocalPlayer():ScreenFade(SCREENFADE.OUT, Color(0, 0, 0), 1.5, 2)
+    surface.PlaySound("teleporter/teleport_sound.ogg")
+    
+    timer.Simple(2, function()
+        local funcCalls, startBlur = {}, SysTime()
+        for i = 0, 30 do 
+            timer.Simple(math.random(0.05, 0.15) * i, function() funcCalls[i] = 0 end)
+        end
+        timer.Create("EffectTimer", 1 / 60, 120, function()
+            for k, v in pairs(funcCalls) do
+                funcCalls[k] = math.Approach(v, 1, 0.01)
+            end
+        end)
+        
+        overlayPanel.Paint = function()
+            draw.NoTexture()
+
+            for k, v in pairs(funcCalls) do
+                surface.SetDrawColor(50, 50, 255 * funcCalls[k], 255)
+                draw.Circle(overlayPanel:GetWide() / 2, overlayPanel:GetTall() / 2, math.sin(v) * 1920, 128)
+            end
+        end
+        overlayPanel2.Paint = function()
+            Derma_DrawBackgroundBlur(overlayPanel2, startBlur)
+            draw.NoTexture()
+            
+            for k, v in pairs(funcCalls) do
+                --funcCalls[k] = math.Approach(v, 1, 0.01)
+                surface.SetTexture(surface.GetTextureID("models/props_combine/com_shield001a"))
+                surface.SetDrawColor(255, 255, 255, 255)
+                draw.Circle(overlayPanel2:GetWide() / 2, overlayPanel2:GetTall() / 2, math.sin(v) * 1920, 128)
+            end
+        end
+
+        timer.Simple(2, function()
+            Screen:Remove()
+        end)
+    end)
+end)
+
+net.Receive("StopOverlay", function()
+    if Screen and ispanel( Screen ) then Screen:Remove() end
+end)
+
+net.Receive("StartBloodCount", function()
+    startedChalk = true
+end)
+
+net.Receive("UpdateBloodCount", function()
+    local updateTo = net.ReadInt(16)
+    chalkMessages.counter.num = updateTo
+    surface.PlaySound("ambient/alarms/warningbell1.wav")
+end)
+
+chalkMessages = {
+    counter = {pos1 = Vector(-568, 3552, 170.5), pos2 = Vector(-402, 3552, 137), num = 0, goal = 30},
+    msg1 = {pos1 = Vector(-1664.0, 2378.5, 125.8), pos2 = Vector(-1664, 2210.75, 58.5), msg = "Blood for the Blood God"},
+    msg2 = {pos1 = Vector(-751.5, 2880.2, 104.2), pos2 = Vector(-630.5, 2880.2, 59.8), msg = "Arcs of Blue make it True"},
+}
+local chalkmaterial = Material("chalk.png", "unlitgeneric smooth")
+
+hook.Add("PostDrawOpaqueRenderables", "DrawChalkMessages", function()
+    if !startedChalk then return end
+
+    --local trace = LocalPlayer():GetEyeTrace()
+    --local angle = trace.HitNormal:Angle()
+
+    local text = ""
+    for k, v in pairs(chalkMessages) do
+        if !v.msg then text = v.num .. " dead"
+        else text = v.msg end
+
+        cam.Start3D2D(v.pos1, Angle(0, 0, 0), 1)
+            surface.SetFont("nz.display.hud.main")
+            surface.SetDrawColor(255, 255, 255)
+            surface.SetMaterial(chalkmaterial)
+            surface.DrawText(text)
+        cam.End3D2D()
+    end
+end)
--- lua/nzmapscripts/nz_rp_neverlosehopehospital_v2u;bloodgod.lua
+++ lua/nzmapscripts/nz_rp_neverlosehopehospital_v2u;bloodgod.lua
@@ -1,27 +1,35 @@
-local mapscript = {}
+util.AddNetworkString("RunFireOverlay")
+util.AddNetworkString("RunTeleportOverlay")
+util.AddNetworkString("StopOverlay")
+util.AddNetworkString("StartBloodCount")
+util.AddNetworkString("UpdateBloodCount")
 
-//The gas cans used to fill the generators, 2 cans per generator
-//lua_run print(Entity(1):GetEyeTrace().Entity:GetPos())
+local mapscript = {}
+mapscript.bloodGodKills = 0
+mapscript.bloodGodKillsGoal = 30
+
+--The gas cans used to fill the generators, 2 cans per generator
+--lua_run print(Entity(1):GetEyeTrace().Entity:GetPos())
 local gascanspawns = {
     { --Can 1, found around the power rooms
-        {pos = Vector(-213.891785 2372.032959 15.332347), ang = Angle(0, -90, 0)},
-        {pos = Vector(-114.092148 2931.946777 14.76), ang = Angle(-36, -98.5, 0)},
-        {pos = Vector(-1923.289063 3534.208740 15.263), ang = Angle(0, 106, 0)}
-    }
+        {pos = Vector(-214, 2372, 15), ang = Angle(0, -90, 0)},
+        {pos = Vector(-114, 2932, 14.75), ang = Angle(-36, -98.5, 0)},
+        {pos = Vector(-1923.25, 3534.25, 15.25), ang = Angle(0, 106, 0)}
+    },
     { --Can 2, found in the bathroom & beyond areas
-        {pos = Vector(-3972.647949 2598.693848 15.330338), ang = Angle(0, -180, 0)},
-        {pos = Vector(-5784.763184 2332.365723 260.4929), ang = Angle(-89.5, -146, -57.5)},
-        {pos = Vector(-5627.165039 2128.645020 15.2646), ang = Angle(0, 0, 0)}
-    }
+        {pos = Vector(-3972.647949, 2598.693848, 15.330338), ang = Angle(0, -180, 0)},
+        {pos = Vector(-5784.763184, 2332.365723, 260.4929), ang = Angle(-89.5, -146, -57.5)},
+        {pos = Vector(-5627.165039, 2128.645020, 15.2646), ang = Angle(0, 0, 0)}
+    },
     { --Can 3, found in the tiled corridors after the long hallways
-        {pos = Vector(-3958.973145 4838.710938 79.33548), ang = Angle(0, 0, 0)},
-        {pos = Vector(-6857.171875 9401.148438 79.33123), ang = Angle(0, 85.5, 0)},
-        {pos = Vector(-6239.565430 7497.475586 79.300950), ang = Angle(0, 178, 0)}
-    }
+        {pos = Vector(-3959, 4838.75, 79), ang = Angle(0, 0, 0)},
+        {pos = Vector(-6857, 9401, 79.3), ang = Angle(0, 85.5, 0)},
+        {pos = Vector(-6239.5, 7497.5, 79.3), ang = Angle(0, 178, 0)}
+    },
     { --Can 4, found in the hospital wing after the tiled corridors
-        {pos = Vector(-5547.892090 10435.421875 79.27690), ang = Angle(0, 82, 0)},
-        {pos = Vector(-6529.118652 9991.431641 83.4996), ang = Angle(26, -109, 0)},
-        {pos = Vector(-4294.641602 10248.747070 79.1447), ang = Angle(-32.5, 136.5, 0)}
+        {pos = Vector(-5548, 10435.5, 79.25), ang = Angle(0, 82, 0)},
+        {pos = Vector(-6529, 9991.5, 83.5), ang = Angle(26, -109, 0)},
+        {pos = Vector(-4294.5, 10248.75, 797), ang = Angle(-32.5, 136.5, 0)}
     }
 }
 
@@ -34,43 +42,59 @@ local generators = {
 --The "pile" of flashlights
 local flashlights = {
 	{	
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+		{pos = Vector(-129.59, 2925.20, 37.49), ang = Angle(-5.13, -178.49, -1.89)},
+		{pos = Vector(-96.75, 2923.43, 37.78), ang = Angle(-5.41, -173.46, 43.86)}
 	},
 	{	
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+		{pos = Vector(-2317.48, 1602.08, 1.13), ang = Angle(-3.56, -12.71, -74.99)},
+		{pos = Vector(-2322.29, 1603.04, 1.76), ang = Angle(-3.38, 174.16, -107.26)},
+		{pos = Vector(-2319.76, 1605.84, 4.06), ang = Angle(90.00, -135.0, 180)}
 	},
 	{	
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+		{pos = Vector(-3070.06, 3085.94, 4.13), ang = Angle(86.38, -171.26, -70.16)},
+		{pos = Vector(-3068.96, 3090.52, 2.40), ang = Angle(-2.93, 121.29, 166.81)},
+		{pos = Vector(-3066.16, 3088.57, 1.22), ang = Angle(-2.76, -60.67, -86.96)}
 	}
 }
 
 --Possible spots players may teleport to on power generator flippage
 local possibleTeleports = {
-	{ --Blocked-off area teleport (otherwise inaccessible)
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0), post = true}
+    { --Blocked-off area teleport (otherwise inaccessible)
+      --Must default to this when neither switch has been flipped, so the player isn't teleported outside a purchased area
+		{pos = Vector(-6751.75, 3268.5, 0), ang = Angle(0, -1800, 0), post = true}
 	},
 	{ --Basement teleport (essentially useless), 3 spots so it feels more genuinely random
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+		{pos = Vector(-3064, 195, -3580), ang = Angle(0, -180, 0)},
+		{pos = Vector(-5082, 724, -3582), ang = Angle(3.5 -17.5, 0)},
+		{pos = Vector(-3604, 2306.5 -3584), ang = Angle(0, -90, 0)}
 	},
 	{ --Random spots in areas that MUST have been purchased thus far (essentially useless), 3 spots again
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)},
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+		{pos = Vector(-5963.5, 2369, -61.5), ang = Angle(0, 0, 0)},
+		{pos = Vector(-1825.5, 3709.75, 0.0), ang = Angle(0, -7.5, 0)},
+		{pos = Vector(-3007.5, 512.5, 0.0), ang = Angle(0, -90, 0)}
 	},
 	{ --"Bunker" area w/ PaP
-		{pos = Vector(0, 0, 0), ang = Angle(0, 0, 0), post = true}
+		{pos = Vector(-2844.5, 297, -1663), ang = Angle(0, 0, 0), post = true}
 	}
 }
+local spawnTeleport = {pos = Vector(-2232, -1028, 2.75), ang = Angle(0, 106.75, 0)}
 
-local spawnTeleport = {pos = Vector(0, 0, 0), ang = Angle(0, 0, 0)}
+local zapRoomEffectLocations = {
+    {
+        {start = Vector(-1216.6, 3552.3, 0), origin = Vector(-1071.4, 3774.0, 192.0)},
+        {start = Vector(-1129.4, 3564.2, 61.4), origin = Vector(-863.7, 3625.4, 54.7)},
+        {start = Vector(-925.9, 3898.5, 0), origin = Vector(-986.9, 3524.3, 123)},
+        {start = Vector(-1056.8, 3771.4, 45.7), origin = Vector(-1075.7, 3552.3, 47.3)},
+        {start = Vector(-1131.1, 3552.6, 45.0), origin = Vector(-1011.8, 3787.6, 0)},
+        {start = Vector(-864.7, 3560.9, 119.2), origin = Vector(-1216.6, 3633.6, 10.5)},
+        {start = Vector(-864.7, 3882.1, 111.6), origin = Vector(-997.4, 3552.3, 49.4)},
+        {start = Vector(-1099.2, 3680.8, 192.0), origin = Vector(-864.7, 3749.5, 5.0)}
+    },
+    {}
+}
+for k, v in pairs(zapRoomEffectLocations[1]) do
+    zapRoomEffectLocations[2][k] = {start = v.start + Vector(895.97, 0, 0), origin = v.origin + Vector(895.97, 0, 0)}
+end
 
 local gascans = nzItemCarry:CreateCategory("gascan")
 gascans:SetIcon("spawnicons/models/props_junk/metalgascan.png") --spawnicons/models/props_junk/gascan001a.png
@@ -126,8 +150,8 @@ gascans:SetCondition( function(self, ply)
 end)
 gascans:Update()
 
-local flashlight = nzItemCarry:CreateCategory("gascan")
-flashlight:SetIcon("spawnicons/") --To set
+local flashlight = nzItemCarry:CreateCategory("flashlight")
+flashlight:SetIcon("spawnicons/zworld_equipment/flashlight.png")
 flashlight:SetText("Press E to pick up a flashlight.")
 flashlight:SetDropOnDowned(false)
 flashlight:SetShowNotification(true)
@@ -141,7 +165,7 @@ flashlight:SetResetFunction(function(self)
 
 	for _, info in pairs(flashlights[math.random(1, #flashlights)]) do
 		local ent = ents.Create("nz_script_prop")
-		ent:SetModel("") --To set
+		ent:SetModel("zworld_equipment/flashlight.mdl")
 		ent:SetPos(info.pos)
 		ent:SetAngles(info.ang)
 		ent:Spawn()
@@ -152,14 +176,15 @@ flashlight:SetResetFunction(function(self)
 end)
 flashlight:SetPickupFunction(function(self, ply, ent)
 	ply:GiveCarryItem(self.id)
-	ply:Flashlight(true)
+    ply:AllowFlashlight(true)
+    mapscript.FlashlightStatuses[ply] = true
 end)
 flashlight:SetCondition( function(self, ply)
 	return !ply:HasCarryItem("flashlight")
 end)
 flashlight:Update()
 
---//Creates the lightning aura once around the given ent
+--//Creates the lightning aura once around the given ent (lasts 0.5 seconds, approximately)
 function Electrify(ent)
 	local effect = EffectData()
 	effect:SetScale(1)
@@ -190,54 +215,73 @@ function SetPermaElectrify(ent, enable)
 	end
 end
 
---How should the effect work? Nothing for 2 seconds then the zap? Do any of the effects follow the zombies as they move?
-function ZapZombies(vec1, vec2, ply)
-	EmitSound("", vec1:Cross(vec2), 1) --Zap sfx
-	timer.Simple(2, function()
-		for _, zom in pairs(ents.FindInBox(vec1, vec2)) do
-			if zom:IsValidZombie() then
+function ZapZombies(id, vec1, vec2, ply)
+    if id == 1 then
+        ents.GetMapCreatedEntity("1556"):EmitSound("misc/charge_up.ogg", 75, 100, 1, CHAN_AUTO)
+    else
+        ents.GetMapCreatedEntity("1650"):EmitSound("misc/charge_up.ogg", 75, 100, 1, CHAN_AUTO)
+    end
+
+    timer.Simple(7, function() --May instead be 7 seconds?
+        for _, info in pairs(zapRoomEffectLocations[id]) do
+            local effectData = EffectData()
+            effectData:SetStart(info.start)
+            effectData:SetOrigin(info.origin)
+            effectData:SetMagnitude( 0.75 )
+            util.Effect("lightning_strike", effectData)
+        end
+
+		for _, ent in pairs(ents.FindInBox(vec1, vec2)) do
+			if ent:IsValidZombie() or ent:IsPlayer() then
 				--Do zombie effect
-				timer.Simple(2, function()
-					if IsValid(zom) then
+				timer.Simple(0.2, function()
+					if IsValid(ent) then
 						local insta = DamageInfo()
 						insta:SetAttacker(ply)
-						insta:SetDamageType() --Need to find an acceptable damage type, old was DMG_BLAST_SURFACE
-						insta:SetDamage(zom:Health())
-						zom:TakeDamageInfo(insta)
+						insta:SetDamageType(DMG_MISSILEDEFENSE) --Need to find an acceptable damage type, old was DMG_BLAST_SURFACE
+						insta:SetDamage(ent:Health())
+						ent:TakeDamageInfo(insta)
 						mapscript.bloodGodKills = mapscript.bloodGodKills + 1
 					end
 				end)
 			end
-		end
+        end
+        
+        timer.Simple(2, function()
+            net.Start("UpdateBloodCount")
+                net.WriteInt(math.Clamp(mapscript.bloodGodKills, 0, 999), 16)
+            net.Broadcast()
+        end)
 	end)
 end
 
+--This function teleports the player to the given pos with the given angle after a possible delay, and plays HUD and sound effects on the client
 function SpecialTeleport(ply, pos, ang, delay)
 	ply:GodEnable()
 	ply:Lock()
-	SetPermaElectrify(ply, true)
+	--SetPermaElectrify(ply, true)
 	timer.Simple(delay or 0, function()
-		--Play build-up sound
+        net.Start("RunTeleportOverlay")
+        net.Send(ply)
 	end)
 
-	timer.Simple(--[[buildupSoundLength]] + (delay or 0), function() --Delay the teleport for a bit to play sounds
+	timer.Simple(2 + (delay or 0), function() --Delay the teleport for a bit to play sound & HUD effect
 		ply:SetNoDraw(true) --may also need to set their equipped weapons invisible
 		--ply:Freeze(true)
-		ply:SetPos(pos)
-		ply:SetAngles(ang)
-		SetPermaElectrify(ply, false)
+		--SetPermaElectrify(ply, false)
 
-		--Do HUD effects
-		--Play sound effects
-
-		timer.Simple(--[[fullEffectLength]] - 1.4, function()
+        --Full length of the HUD effects should be 4 seconds
+		timer.Simple(2 - 1.4, function()
 			local effectData = EffectData()
 			effectData:SetOrigin(pos)
 			effectData:SetMagnitude(2)
 			effectData:SetEntity(nil)
 			util.Effect("lightning_prespawn", effectData)
 
-			timer.Simple(1.4, function()
+            timer.Simple(1.4, function()
+                ply:SetPos(pos)
+                ply:SetAngles(ang)
+        
 				effectData = EffectData()
 				effectData:SetStart( ply:GetPos() + Vector(0, 0, 1000) )
 				effectData:SetOrigin( ply:GetPos() )
@@ -246,11 +290,12 @@ function SpecialTeleport(ply, pos, ang, delay)
 
 				ply:SetNoDraw(false)
 				ply:GodDisable()
-				ply:UnLock()
+                ply:UnLock()
+                --Alternative idea to changing the collision group, we could also just kill the zombies in a box around it
 				timer.Simple(1, function() --We don't want the player spawning inside a zombie and not being able to move
 					ply:SetCollisionGroup(COLLISION_GROUP_NONE) --TO CHECK
 					timer.Simple(1, function()
-						ply:SetCollisionGroup() --Change back
+						ply:SetCollisionGroup(COLLISION_GROUP_PLAYER) --Change back
 					end)
 				end)
 			end)
@@ -260,33 +305,49 @@ end
 
 function mapscript.OnGameBegin()
     gascans:Reset()
-	flashlight:Reset()
+    flashlight:Reset()
+    mapscript.FlashlightStatuses = {}
+
+    timer.Simple( 0, function()
+        for k, v in pairs( player.GetAll() ) do
+            mapscript.FlashlightStatuses[ v ] = false
+            v:AllowFlashlight( false )
+        end
+    end )
     
+    timer.Simple(3, function()
+        for k, v in pairs(player.GetAll()) do
+            --[[if v:Alive() then
+                v:SendLua("surface.PlaySound(\"misc/evilgiggle.ogg)\"")
+            end]]
+        end
+    end)
     --Need to lock the elevator doors & buttons here
 	mapscript.bloodGodKills = 0
 	local initialUse = false
 	local killSwitch = ents.GetMapCreatedEntity("1556") --Non-bloody room button
-	killSwitch.OnUsed = function(ply)
-		if !nzElec:IsOn() or switchOneOn or switchTwoOn then
-			return false
+	killSwitch.OnUsed = function(but, ply)
+        if !nzElec:IsOn() --[[or switchOneOn or switchTwoOn]] then
+            --but:EmitSound("ambient/buttons/button2.wav") - Already emits this sound
+			return
 		end
 
-		switchOneOn = true
-		--Do room effect
-		ZapZombies(Vector(-884.5, 3540, 64), Vector(-1200.5, 3888, 64), ply)
+        but:EmitSound("buttons/button24.wav")
+        switchOneOn = true
+        ZapZombies(1, Vector(-884.5, 3540, 64), Vector(-1200.5, 3888, 64), ply)
 	end
 	killSwitch = ents.GetMapCreatedEntity("1650") --Bloody room button
-	killSwitch.OnUsed = function(ply)
-        if !nzElec:IsOn() or switchOneOn or switchTwoOn then
+	killSwitch.OnUsed = function(but, ply)
+        if !nzElec:IsOn() --[[or switchOneOn or switchTwoOn]] then
 			return false
 		end
         
         switchTwoOn = true
-        ZapZombies(Vector(11, 3540, 64), Vector(-304.5, 3888, 64), ply)
+        ZapZombies(2, Vector(11, 3540, 64), Vector(-304.5, 3888, 64), ply)
 	end
 
 	local sparkLever = ents.GetMapCreatedEntity("1921")
-	sparkLever.OnUsed = function(ply)
+	sparkLever.OnUsed = function(but, ply)
 		if !sparkFlipped then
 			sparkFlipped = true
 		else
@@ -296,7 +357,7 @@ function mapscript.OnGameBegin()
 	end
 
 	local nonSparkLever = ents.GetMapCreatedEntity("1920")
-	nonSparkLever = function(ply)
+	nonSparkLever.OnUsed = function(but, ply)
 		if !nonSparkFlipped then
 			nonSparkFlipped = true
 		else
@@ -317,10 +378,17 @@ function mapscript.OnGameBegin()
 
 	--The generator power switch that teleports the player
 	newPowerSwitch = ents.GetMapCreatedEntity("2767")
-	newPowerSwitch.OnUsed = function(ply)
+    newPowerSwitch.OnUsed = function(but, ply)
+        if delay then return end
+
+        local delay = true
+        SetPermaElectrify(but, true)
+        Electrify(ply)
+        timer.Simple(30, function() delay = false SetPermaElectrify(but, false) end)
+
 		if nzElec:IsOn() then
 			--Since a solo player may explore this area before doing anything with the EE, "punish" them instead
-			--Do some effect and damage the player
+			--Do some sound effect and damage the player
 		end
 
 		local teleportAgain, randomValue = false
@@ -336,13 +404,13 @@ function mapscript.OnGameBegin()
             end
         elseif !sparkFlipped and nonSparkFlipped then
             randomValue = math.random(#nonSparkFlippedOption)
-            SpecialTeleport(ply, nonSparkFlippedOption[randomValue].pos, nonSparkFlippedOption[randomValue)].ang, 1)
+            SpecialTeleport(ply, nonSparkFlippedOption[randomValue].pos, nonSparkFlippedOption[randomValue].ang, 1)
             if nonSparkFlippedOption.post then
                 teleportAgain = true
             end
         else
             randomValue = math.random(#bothFlippedOption)
-            SpecialTeleport(ply, bothFlippedOption[randomValue].pos, bothFlippedOption[randomValue)].ang, 1)
+            SpecialTeleport(ply, bothFlippedOption[randomValue].pos, bothFlippedOption[randomValue].ang, 1)
             if bothFlippedOption.post then
                 teleportAgain = true
             end
@@ -352,13 +420,13 @@ function mapscript.OnGameBegin()
             teleportTimers = teleportTimers or {}
             teleportTimers[ply:SteamID()] = 120 + math.random(-30, 30)
 
-            timer.Create(ply:SteamID() + "TeleportTimer", 1, 0, function()
+            timer.Create(ply:SteamID() .. "TeleportTimer", 1, 0, function()
                 if !teleportTimers[ply:SteamID()] or !IsValid(ply) then 
-                    timer.Remove(ply:SteamID() + "TeleportTimer")
+                    timer.Remove(ply:SteamID() .. "TeleportTimer")
                 end
-                if teleportTimers[ply:SteamID()] == 0 or !ply:GetNotDowned then
-                    timer.Remove(ply:SteamID() + "TeleportTimer")
-                    SpecialTeleport(ply, Vector(), Angle()) --Should be back to spawn
+                if teleportTimers[ply:SteamID()] == 0 or !ply:GetNotDowned() then
+                    timer.Remove(ply:SteamID() .. "TeleportTimer")
+                    SpecialTeleport(ply, spawnTeleport.pos, spawnTeleport.ang)
                 end
 				--[[if teleportTimers[ply:SteamID()] == 10 then --or some number 
 					--Do something
@@ -367,12 +435,8 @@ function mapscript.OnGameBegin()
 				teleportTimers[ply:SteamID()] = teleportTimers[ply:SteamID()] - 1
             end)
         end
+        return true
 	end
-
-    for _, ply in pairs(player.GetAll()) do
-        ply:Flashlight(false)
-        --ply:AllowFlashlight(false)
-    end
 
 	--Creates the 2 generators
     for num, tab in pairs(generators) do
@@ -446,13 +510,27 @@ function mapscript.OnGameBegin()
 			end
 		end
 	end
+end
 
+function mapscript.OnRoundStart()
+    timer.Simple(0, function()
+        for k, v in pairs(player.GetAll()) do
+            if mapscript.FlashlightStatuses[v] then 
+                v:AllowFlashlight(true)
+            else 
+                mapscript.FlashlightStatuses[v] = false
+                v:AllowFlashlight(false)
+            end
 
+            net.Start("StartBloodCount")
+            net.Send(v)
+        end
+    end )
 end
 
 function mapscript.ElectricityOn()
 	if !postFirstActivation then
-		--Remove the grayscale effect here
+		ents.FindByClass("edit_color")[1]:SetContrast(1)
 
 		timer.Simple(5, function()
 			--Move the power switch outside of the map and replace it with a decoy
@@ -463,6 +541,8 @@ function mapscript.ElectricityOn()
 end
 
 --Need to delete the decoy power switch on game end
+
+return mapscript
 
 /*
 Test Effects:
@@ -481,7 +561,8 @@ Puzzle Ideas:
 					Maybe under certain conditions it teleports to bunker area
 	- Messages should hint at easter egg steps, using chalk effect from wallbuys
 	- Simple key & lock puzzle?
-	- 
+    - https://wiki.facepunch.com/gmod/navmesh
+    - https://wiki.facepunch.com/gmod/CNavArea
 
 Useful EE function:
 	nzNotifications:PlaySound("")
@@ -495,5 +576,7 @@ Entity IDs:
     Basement console: 2056
     Basement lever: 1920
     Basement radio: 2144
-    Basement lever (sparking): 1921
+    Basement lever (sparking): 1921 (first one)
+    Bunker console: 1359
+    Bunker radio: 1403
 */
